1a45350e394751a42cdc8a79d50ed89b19fb39df
Glauton Santos <glautoncardoso@gmail.com>
2021-02-26 13:26:02 -0300

    Refactoring ClassListPage style

---
+++ src/pages/ClassListPage/styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F4F5F9',
+    justifyContent: 'center',
+    marginTop: 70,
+    marginLeft: 10,
+    marginRight: 10
+  },
+  headerText: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    marginLeft: 10,
+    color: '#363636'
+  }
+});
+
+export default styles
