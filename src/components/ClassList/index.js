f6475d3252cfa96bb5ef9e7f46c521a1051468ed
GustavoBR44 <gustavofechine@gmail.com>
2021-04-07 11:05:58 -0300

    Subjects are now loaded from firestore

--- src/components/ClassList/index.js
+++ src/components/ClassList/index.js
@@ -3,9 +3,21 @@ import styles from './styles'
 import { SafeAreaView, View, FlatList, Text } from 'react-native';
 import Card from '../Card'
 
+import firebase from '../../classes/firebase';
+
 import DATA from '../../api/subjects.json'
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+const loadSubjects = (updateList) => {
+  firebase.getItems().then((itemList) => {
+    updateList(itemList);
+  });
+}
 
 const ClassList = ({ iconEvent, navigation }) => {
+  const [subjectList, updateSunbjectList] = useState([]);
+
   const renderCard = ({ item }) => (
     <Card
       subjectName={item.subjectName}
@@ -18,10 +30,14 @@ const ClassList = ({ iconEvent, navigation }) => {
     />
   );
 
+  useEffect(() => {
+    loadSubjects(updateSunbjectList);
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
-        data={DATA}
+        data={subjectList}
         renderItem={renderCard}
         keyExtractor={card => card.id}
       />
